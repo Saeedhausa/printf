bf4ae1419ce5dc033af3166a13a6dd0624eab184
Baba Innocent Aganah <69471685+iAganah@users.noreply.github.com>
2023-04-25 01:11:08 -0500

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+/***************First Team Work***************/
 /**
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
